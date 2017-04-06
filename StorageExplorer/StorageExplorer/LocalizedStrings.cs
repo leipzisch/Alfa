f0bf07151d3399d5d4473e840703c93a6fb3a654
leipzisch <tontchev@web.de>
2017-04-06 10:53:09 +0200

    Initial Commit

---
+++ StorageExplorer/StorageExplorer/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using StorageExplorer.Resources;
+
+namespace StorageExplorer
+{
+    /// <summary>
+    /// Bietet Zugriff auf Zeichenfolgenressourcen.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
